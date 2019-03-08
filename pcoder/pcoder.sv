58abe30c4b7b80092f767af0c20275b22e104c08
Anna <45363539+cdrann@users.noreply.github.com>
2019-03-09 00:03:20 +0700

    Update pcoder.sv

--- pcoder/pcoder.sv
+++ pcoder/pcoder.sv
@@ -28,13 +28,13 @@ module pcoder(
 always_comb
     casex(in)
         8'b1xxxxxxx : code <= 3'b111;
-        8'b1xxxxxxx : code <= 3'b110;
-        8'b1xxxxxxx : code <= 3'b101;
-        8'b1xxxxxxx : code <= 3'b100;
-        8'b1xxxxxxx : code <= 3'b011;
-        8'b1xxxxxxx : code <= 3'b010;
-        8'b1xxxxxxx : code <= 3'b001;
-        8'b1xxxxxxx : code <= 3'b000;
+        8'b01xxxxxx : code <= 3'b110;
+        8'b001xxxxx : code <= 3'b101;
+        8'b0001xxxx : code <= 3'b100;
+        8'b00001xxx : code <= 3'b011;
+        8'b000001xx : code <= 3'b010;
+        8'b0000001x : code <= 3'b001;
+        8'b00000000 : code <= 3'b000;
         default     : code <= 3'b000;            
     endcase 
   
