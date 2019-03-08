0d2a589c05a94eac08ecb5bfdfef112d3804f76a
Anna <45363539+cdrann@users.noreply.github.com>
2019-03-08 23:36:13 +0700

    Update muxer8.sv

--- muxer8/muxer8.sv
+++ muxer8/muxer8.sv
@@ -25,17 +25,7 @@ module muxer8(
     input logic [2:0] sel,
     output logic q
     );
-    
-    /* assign q = !sel[0] & !sel[1] & !sel[2] & in[0]
-             | !sel[0] & !sel[1] &  sel[2] & in[1]
-             | !sel[0] &  sel[1] & !sel[2] & in[5]
-             | !sel[0] &  sel[1] &  sel[2] & in[6]
-             |  sel[0] & !sel[1] & !sel[2] & in[4]
-             |  sel[0] & !sel[1] &  sel[2] & in[5]
-             |  sel[0] &  sel[1] & !sel[2] & in[6]
-             |  sel[0] &  sel[1] &  sel[2] & in[7];
-        */
-        
+ 
         always_comb
             case(sel)
                 3'b000 : q <= in[0]; 
