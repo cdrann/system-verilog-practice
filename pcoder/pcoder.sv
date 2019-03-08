7703be8552c7e8fb3f04b5e5a27705c5cf3163e3
Anna <45363539+cdrann@users.noreply.github.com>
2019-03-09 00:02:01 +0700

    Update pcoder.sv

--- pcoder/pcoder.sv
+++ pcoder/pcoder.sv
@@ -35,8 +35,27 @@ always_comb
         8'b1xxxxxxx : code <= 3'b010;
         8'b1xxxxxxx : code <= 3'b001;
         8'b1xxxxxxx : code <= 3'b000;
-        default     : code <= 3'b000 
-                
+        default     : code <= 3'b000;            
     endcase 
   
+   /* if (in[7])
+        code <= 3'b111;
+    else if (in[6])
+        code <= 3'b110;
+    else if (in[5])
+        code <= 3'b101;
+    else if (in[4])
+        code <= 3'b100;
+    else if (in[3])
+        code <= 3'b011;
+    else if (in[2])
+        code <= 3'b010;
+    else if (in[1])
+        code <= 3'b001;
+    else if (in[0])
+        code <= 3'b000;
+    else 
+        code <= 3'b000;
+        */
+    
 endmodule
