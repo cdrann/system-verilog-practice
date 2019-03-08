79743eca5e8a8a91388a823194ce3941d07fa77e
Anna <45363539+cdrann@users.noreply.github.com>
2019-03-09 00:24:49 +0700

    Update muxer8.sv

--- muxer8/muxer8.sv
+++ muxer8/muxer8.sv
@@ -26,16 +26,16 @@ module muxer8(
     output logic q
     );
  
-        always_comb
-            case(sel)
-                3'b000 : q <= in[0]; 
-                3'b001 : q <= in[1]; 
-                3'b010 : q <= in[2]; 
-                3'b011 : q <= in[3]; 
-                3'b100 : q <= in[4]; 
-                3'b101 : q <= in[5]; 
-                3'b110 : q <= in[6]; 
-                3'b111 : q <= in[7]; 
-            endcase 
+always_comb
+    case(sel)
+        3'b000 : q <= in[0]; 
+        3'b001 : q <= in[1]; 
+        3'b010 : q <= in[2]; 
+        3'b011 : q <= in[3]; 
+        3'b100 : q <= in[4]; 
+        3'b101 : q <= in[5]; 
+        3'b110 : q <= in[6]; 
+        3'b111 : q <= in[7]; 
+    endcase 
   
 endmodule
