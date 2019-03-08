aef9bdc895fdf6a8151413f421fdcde5f6dcad2d
Anna <45363539+cdrann@users.noreply.github.com>
2019-03-09 01:38:34 +0700

    Update muxer16.sv

--- muxer 16/muxer16.sv
+++ muxer 16/muxer16.sv
@@ -28,11 +28,11 @@ module muxer16(
     
 logic [3:0] tmp;
    
-muxer4 m0(.in(in[3:0]), .sel(sel[0]), .q(tmp[0]));
-muxer4 m1(.in(in[7:4]), .sel(sel[0]), .q(tmp[1]));
-muxer4 m2(.in(in[11:8]), .sel(sel[0]), .q(tmp[2]));
-muxer4 m3(.in(in[15:12]), .sel(sel[0]), .q(tmp[3]));
+    muxer4 m0(.in(in[3:0]), .sel(sel[1:0]), .q(tmp[0]));
+    muxer4 m1(.in(in[7:4]), .sel(sel[1:0]), .q(tmp[1]));
+    muxer4 m2(.in(in[11:8]), .sel(sel[1:0]), .q(tmp[2]));
+    muxer4 m3(.in(in[15:12]), .sel(sel[1:0]), .q(tmp[3]));
 
-muxer4 m4(.in(tmp), .sel(sel[1]), .q(q));
+    muxer4 m4(.in(tmp), .sel(sel[3:2]), .q(q));
 
 endmodule
